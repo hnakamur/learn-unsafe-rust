4b03c093751b3ad5c1799d71d1c6eb7fb4af3c0c
Hiroaki Nakamura <hnakamur@gmail.com>
2022-04-25 14:41:58 +0900

    Copy the final version of Vec from the book

--- src/vec.rs
+++ src/vec.rs
@@ -4,108 +4,6 @@ use std::mem;
 use std::ops::{Deref, DerefMut};
 use std::ptr::{self, NonNull};
 
-pub struct Vec<T> {
-    buf: RawVec<T>,
-    len: usize,
-}
-
-unsafe impl<T: Send> Send for Vec<T> {}
-unsafe impl<T: Sync> Sync for Vec<T> {}
-
-impl<T> Vec<T> {
-    pub fn new() -> Self {
-        Vec {
-            buf: RawVec::new(),
-            len: 0,
-        }
-    }
-
-    pub fn push(&mut self, elem: T) {
-        if self.len == self.cap() {
-            self.buf.grow();
-        }
-
-        unsafe {
-            ptr::write(self.ptr().add(self.len), elem);
-        }
-
-        // Can't fail, we'll OOM first.
-        self.len += 1;
-    }
-
-    pub fn pop(&mut self) -> Option<T> {
-        if self.len == 0 {
-            None
-        } else {
-            self.len -= 1;
-            unsafe { Some(ptr::read(self.ptr().add(self.len))) }
-        }
-    }
-
-    pub fn insert(&mut self, index: usize, elem: T) {
-        // Note: `<=` because it's valid to insert after everything
-        // which would be equivalent to push.
-        assert!(index <= self.len, "index out of bounds");
-        if self.cap() == self.len {
-            self.buf.grow();
-        }
-
-        unsafe {
-            // ptr::copy(src, dest, len): "copy from src to dest len elems"
-            ptr::copy(
-                self.ptr().add(index),
-                self.ptr().add(index + 1),
-                self.len - index,
-            );
-            ptr::write(self.ptr().add(index), elem);
-            self.len += 1;
-        }
-    }
-
-    pub fn remove(&mut self, index: usize) -> T {
-        // Note: `<` because it's *not* valid to remove after everything
-        assert!(index < self.len, "index out of bounds");
-        unsafe {
-            self.len -= 1;
-            let result = ptr::read(self.ptr().add(index));
-            ptr::copy(
-                self.ptr().add(index + 1),
-                self.ptr().add(index),
-                self.len - index,
-            );
-            result
-        }
-    }
-
-    fn ptr(&self) -> *mut T {
-        self.buf.ptr.as_ptr()
-    }
-
-    fn cap(&self) -> usize {
-        self.buf.cap
-    }
-}
-
-impl<T> Drop for Vec<T> {
-    fn drop(&mut self) {
-        while let Some(_) = self.pop() {}
-        // deallocation is handled by RawVec
-    }
-}
-
-impl<T> Deref for Vec<T> {
-    type Target = [T];
-    fn deref(&self) -> &[T] {
-        unsafe { std::slice::from_raw_parts(self.ptr(), self.len) }
-    }
-}
-
-impl<T> DerefMut for Vec<T> {
-    fn deref_mut(&mut self) -> &mut [T] {
-        unsafe { std::slice::from_raw_parts_mut(self.ptr(), self.len) }
-    }
-}
-
 struct RawVec<T> {
     ptr: NonNull<T>,
     cap: usize,
@@ -184,16 +82,135 @@ impl<T> Drop for RawVec<T> {
     }
 }
 
+pub struct Vec<T> {
+    buf: RawVec<T>,
+    len: usize,
+}
+
+impl<T> Vec<T> {
+    fn ptr(&self) -> *mut T {
+        self.buf.ptr.as_ptr()
+    }
+
+    fn cap(&self) -> usize {
+        self.buf.cap
+    }
+
+    pub fn new() -> Self {
+        Vec {
+            buf: RawVec::new(),
+            len: 0,
+        }
+    }
+    pub fn push(&mut self, elem: T) {
+        if self.len == self.cap() {
+            self.buf.grow();
+        }
+
+        unsafe {
+            ptr::write(self.ptr().add(self.len), elem);
+        }
+
+        // Can't overflow, we'll OOM first.
+        self.len += 1;
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        if self.len == 0 {
+            None
+        } else {
+            self.len -= 1;
+            unsafe { Some(ptr::read(self.ptr().add(self.len))) }
+        }
+    }
+
+    pub fn insert(&mut self, index: usize, elem: T) {
+        assert!(index <= self.len, "index out of bounds");
+        if self.cap() == self.len {
+            self.buf.grow();
+        }
+
+        unsafe {
+            ptr::copy(
+                self.ptr().add(index),
+                self.ptr().add(index + 1),
+                self.len - index,
+            );
+            ptr::write(self.ptr().add(index), elem);
+            self.len += 1;
+        }
+    }
+
+    pub fn remove(&mut self, index: usize) -> T {
+        assert!(index < self.len, "index out of bounds");
+        unsafe {
+            self.len -= 1;
+            let result = ptr::read(self.ptr().add(index));
+            ptr::copy(
+                self.ptr().add(index + 1),
+                self.ptr().add(index),
+                self.len - index,
+            );
+            result
+        }
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        unsafe {
+            let iter = RawValIter::new(&self);
+            let buf = ptr::read(&self.buf);
+            mem::forget(self);
+
+            IntoIter {
+                iter: iter,
+                _buf: buf,
+            }
+        }
+    }
+
+    pub fn drain(&mut self) -> Drain<T> {
+        unsafe {
+            let iter = RawValIter::new(&self);
+
+            // this is a mem::forget safety thing. If Drain is forgotten, we just
+            // leak the whole Vec's contents. Also we need to do this *eventually*
+            // anyway, so why not do it now?
+            self.len = 0;
+
+            Drain {
+                iter: iter,
+                vec: PhantomData,
+            }
+        }
+    }
+}
+
+impl<T> Drop for Vec<T> {
+    fn drop(&mut self) {
+        while let Some(_) = self.pop() {}
+        // deallocation is handled by RawVec
+    }
+}
+
+impl<T> Deref for Vec<T> {
+    type Target = [T];
+    fn deref(&self) -> &[T] {
+        unsafe { std::slice::from_raw_parts(self.ptr(), self.len) }
+    }
+}
+
+impl<T> DerefMut for Vec<T> {
+    fn deref_mut(&mut self) -> &mut [T] {
+        unsafe { std::slice::from_raw_parts_mut(self.ptr(), self.len) }
+    }
+}
+
 struct RawValIter<T> {
     start: *const T,
     end: *const T,
 }
 
 impl<T> RawValIter<T> {
-    // unsafe to construct because it has no associated lifetimes.
-    // This is necessary to store a RawValIter in the same struct as
-    // its actual allocation. OK since it's a private implementation
-    // detail.
     unsafe fn new(slice: &[T]) -> Self {
         RawValIter {
             start: slice.as_ptr(),
@@ -204,49 +221,6 @@ impl<T> RawValIter<T> {
             } else {
                 slice.as_ptr().add(slice.len())
             },
-        }
-    }
-}
-
-pub struct IntoIter<T> {
-    _buf: RawVec<T>, // we don't actually care about this. Just need it to live.
-    iter: RawValIter<T>,
-}
-
-impl<T> Iterator for IntoIter<T> {
-    type Item = T;
-    fn next(&mut self) -> Option<T> {
-        self.iter.next()
-    }
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.iter.size_hint()
-    }
-}
-
-impl<T> DoubleEndedIterator for IntoIter<T> {
-    fn next_back(&mut self) -> Option<T> {
-        self.iter.next_back()
-    }
-}
-
-impl<T> Drop for IntoIter<T> {
-    fn drop(&mut self) {
-        for _ in &mut *self {}
-    }
-}
-
-impl<T> Vec<T> {
-    pub fn into_iter(self) -> IntoIter<T> {
-        unsafe {
-            let iter = RawValIter::new(&self);
-
-            let buf = ptr::read(&self.buf);
-            mem::forget(self);
-
-            IntoIter {
-                iter: iter,
-                _buf: buf,
-            }
         }
     }
 }
@@ -294,6 +268,33 @@ impl<T> DoubleEndedIterator for RawValIter<T> {
     }
 }
 
+pub struct IntoIter<T> {
+    _buf: RawVec<T>, // we don't actually care about this. Just need it to live.
+    iter: RawValIter<T>,
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
+        self.iter.next()
+    }
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.iter.size_hint()
+    }
+}
+
+impl<T> DoubleEndedIterator for IntoIter<T> {
+    fn next_back(&mut self) -> Option<T> {
+        self.iter.next_back()
+    }
+}
+
+impl<T> Drop for IntoIter<T> {
+    fn drop(&mut self) {
+        for _ in &mut *self {}
+    }
+}
+
 pub struct Drain<'a, T: 'a> {
     vec: PhantomData<&'a mut Vec<T>>,
     iter: RawValIter<T>,
@@ -317,25 +318,8 @@ impl<'a, T> DoubleEndedIterator for Drain<'a, T> {
 
 impl<'a, T> Drop for Drain<'a, T> {
     fn drop(&mut self) {
+        // pre-drain the iter
         for _ in &mut *self {}
-    }
-}
-
-impl<T> Vec<T> {
-    pub fn drain(&mut self) -> Drain<T> {
-        unsafe {
-            let iter = RawValIter::new(&self);
-
-            // this is a mem::forget safety thing. If Drain is forgotten, we just
-            // leak the whole Vec's contents. Also we need to do this *eventually*
-            // anyway, so why not do it now?
-            self.len = 0;
-
-            Drain {
-                iter: iter,
-                vec: PhantomData,
-            }
-        }
     }
 }
 
@@ -344,46 +328,69 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        println!("It works!");
+    pub fn create_push_pop() {
+        let mut v = Vec::new();
+        v.push(1);
+        assert_eq!(1, v.len());
+        assert_eq!(1, v[0]);
+        for i in v.iter_mut() {
+            *i += 1;
+        }
+        v.insert(0, 5);
+        let x = v.pop();
+        assert_eq!(Some(2), x);
+        assert_eq!(1, v.len());
+        v.push(10);
+        let x = v.remove(0);
+        assert_eq!(5, x);
+        assert_eq!(1, v.len());
     }
 
     #[test]
-    fn push_and_pop() {
-        let mut vec = Vec::new();
-        vec.push(String::from("a"));
-        vec.push(String::from("b"));
-        assert_eq!(Some(String::from("b")), vec.pop());
-        assert_eq!(Some(String::from("a")), vec.pop());
-        assert_eq!(None, vec.pop());
+    pub fn iter_test() {
+        let mut v = Vec::new();
+        for i in 0..10 {
+            v.push(Box::new(i))
+        }
+        let mut iter = v.into_iter();
+        let first = iter.next().unwrap();
+        let last = iter.next_back().unwrap();
+        drop(iter);
+        assert_eq!(0, *first);
+        assert_eq!(9, *last);
     }
 
     #[test]
-    fn iterator() {
-        let mut vec = Vec::new();
-        vec.push(String::from("a"));
-        vec.push(String::from("b"));
-
-        let mut it = vec.iter();
-        assert_eq!(Some(&String::from("a")), it.next());
-        assert_eq!(Some(&String::from("b")), it.next());
-        assert_eq!(None, it.next());
+    pub fn test_drain() {
+        let mut v = Vec::new();
+        for i in 0..10 {
+            v.push(Box::new(i))
+        }
+        {
+            let mut drain = v.drain();
+            let first = drain.next().unwrap();
+            let last = drain.next_back().unwrap();
+            assert_eq!(0, *first);
+            assert_eq!(9, *last);
+        }
+        assert_eq!(0, v.len());
+        v.push(Box::new(1));
+        assert_eq!(1, *v.pop().unwrap());
     }
 
-    #[derive(Debug, PartialEq)]
-    struct Nothing;
-
     #[test]
-    fn zero_sized_type_iterator() {
-        let mut vec = Vec::new();
-        vec.push(Nothing);
-        vec.push(Nothing);
-        vec.push(Nothing);
+    pub fn test_zst() {
+        let mut v = Vec::new();
+        for _i in 0..10 {
+            v.push(())
+        }
 
-        let mut it = vec.into_iter();
-        assert_eq!(Some(Nothing), it.next());
-        assert_eq!(Some(Nothing), it.next());
-        assert_eq!(Some(Nothing), it.next());
-        assert_eq!(None, it.next());
+        let mut count = 0;
+
+        for _ in v.into_iter() {
+            count += 1
+        }
+
+        assert_eq!(10, count);
     }
 }
